57fdc6c3617b7edb420d193a49640c0c8dac2adf
davidomej <davidomej@hotmail.com>
2023-11-30 22:02:38 +0000

    Change url apiEmail in envaironmente

--- e-can-frontend/src/environments/environment.ts
+++ e-can-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://e-can.azurewebsites.net/',
-    apiEmail: 'http://localhost:3000/sendEmail'
+    apiEmail: 'https://e-can.azurewebsites.net/sendEmail'
   };
   
